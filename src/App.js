0d40d102fbb72811172e1ee3183c29c1caacccd9
safronman <safronmanbox@gmail.com>
2019-12-10 12:57:23 +0300

    lesson-2-task-6

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import ToDoListTasks from "./ToDoListTasks";
 import ToDoListFooter from "./ToDoListFooter";
 
 class App extends React.Component {
+    tasks = [
+        {title: 'CSS', isDone: true, priority: 'High'},
+        {title: 'HTML', isDone: true, priority: 'High'},
+        {title: 'JS', isDone: false, priority: 'Low'},
+        {title: 'ReactJS', isDone: true, priority: 'Low'},
+        {title: 'Yo', isDone: false, priority: 'Low'}
+    ];
     render = () => {
-        let tasks = [
-            {title: 'CSS', isDone: true, priority: 'High'},
-            {title: 'HTML', isDone: true, priority: 'High'},
-            {title: 'JS', isDone: false, priority: 'Medium'},
-            {title: 'ReactJS', isDone: true, priority: 'Low'},
-            {title: 'Yo', isDone: false, priority: 'Low'}
-        ];
         return (
             <div className="App">
                 <div className="todoList">
                     <ToDoListHeader/>
-                    <ToDoListTasks tasks={tasks}/>
+                    <ToDoListTasks tasks={this.tasks}/>
                     <ToDoListFooter/>
                 </div>
             </div>
