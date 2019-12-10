614d932826de5c703ea8dddc34b79a7fea313399
safronman <safronmanbox@gmail.com>
2019-12-10 12:29:23 +0300

    lesson-2-task-3

--- src/ToDoListTasks.js
+++ src/ToDoListTasks.js
@@ -5,14 +5,20 @@ import ToDoListTask from "./ToDoListTask";
 
 class ToDoListTasks extends React.Component {
     render = () => {
-        return (
-            <div>
-                <ToDoListTask title="CSS" isDone={false}/>
-                <ToDoListTask title="JS" isDone={true}/>
-                <ToDoListTask title="HTML" isDone={true}/>
-                <ToDoListTask title="ReactJS" isDone={false}/>
-            </div>
 
+        let tasks = [
+            {title: 'CSS', isDone: true},
+            {title: 'HTML', isDone: false},
+            {title: 'JS', isDone: false},
+            {title: 'ReactJS', isDone: true}
+        ];
+        return (
+            <div className="todoList-tasks">
+                <ToDoListTask title={tasks[0].title} isDone={tasks[0].isDone}/>
+                <ToDoListTask title={tasks[1].title} isDone={tasks[1].isDone}/>
+                <ToDoListTask title={tasks[2].title} isDone={tasks[2].isDone}/>
+                <ToDoListTask title={tasks[3].title} isDone={tasks[3].isDone}/>
+            </div>
         );
     }
 }
